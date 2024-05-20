3ddcebb40284a1a8af97f02031ca7cf99554d994
Nic <nicctang@gmail.com>
2024-05-20 13:14:36 +0000

    Updates to ASR

--- asr/src/ASRManager.py
+++ asr/src/ASRManager.py
@@ -1,8 +1,24 @@
+import torch
+import torchaudio
+import whisper 
+from datasets import load_dataset
+
+dataset = load_dataset("/home/jupyter/novice/audio/audio_0.wav")
+
+# audio = whisper.load_audio(audio_path)
+# audio = whisper.pad_or_trim(audio)
+# model = whisper.load_model("tiny").to('cuda')
+# mel = whisper.log_mel_spectrogram(audio).to('cuda')
+# options = whisper.DecodingOptions()
+# result = whisper.decode(model, mel, options)
+print(dataset)
+
 class ASRManager:
     def __init__(self):
-        # initialize the model here
+        # self.model = whisper.load_model("tiny.en")
         pass
 
     def transcribe(self, audio_bytes: bytes) -> str:
-        # perform ASR transcription
-        return ""
+        # return self.model.transcribe(audio_bytes)
+        return '';
+    
